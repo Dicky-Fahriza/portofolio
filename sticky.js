65e654b71151edbf7a14f314fea4a1b764276229
Dicky-Fahriza <87850084+Dicky-Fahriza@users.noreply.github.com>
2022-04-16 02:14:50 +0700

    Add files via upload

---
+++ sticky.js
@@ -0,0 +1,30 @@
+// Sticky navigation bar function on scroll
+window.onscroll = function() { stickyScroll() };
+
+// Navigation bar
+var navbar = document.getElementById('navbar');
+            
+// Sticky measures how much offset there is between
+// nav bar dan top of display
+var sticky = navbar.offsetTop;
+
+function stickyScroll() {
+    // Measures the vertical offset between the navbar
+ // and the display top
+    if (window.pageYOffset >= sticky) {
+        // Appends the class sticky on the navigation bar
+        navbar.classList.add("sticky");
+    } else {
+         // Removes the class sticky from the navigation bar
+        navbar.classList.remove("sticky");
+    }
+ }
+
+var counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 4){
+        counter = 1;
+      }
+    }, 5000);
